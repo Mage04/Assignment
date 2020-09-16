2f1b1e8b8b7bd6872be13d5266fa68f4376eed32
Mage04 <mahageetha04@gmail.com>
2020-09-16 05:49:21 +0530

    Add files via upload

---
+++ Sept13/IIFE/odd_IIFE.js
@@ -0,0 +1,13 @@
+ (function ()
+{
+ var a=[1,2,3,4,5];
+ 
+
+for (var i = 0; i < a.length; i++) {
+            if (a[i] % 2 !== 0) {
+            console.log(a[i]);
+            }
+        }
+    })();
+
+  
---
+++ Sept13/IIFE/title_IIFE.js
@@ -0,0 +1,16 @@
+
+ (function()
+   {
+   	var content =['hello WorLd','JavaSripT','progRAM'];
+   	for(var i = 0; i< content.length; i++){
+
+var text = content[i].toUpperCase();
+
+console.log(text.split(' ').join(" "));
+
+//return content;
+   	}
+
+
+   })
+   ();
