2230cc11ad4f6570dc4d847574d2ec482efc7e00
Mage04 <mahageetha04@gmail.com>
2020-09-16 05:45:58 +0530

    Add files via upload

---
+++ Sept13/Anonymous function/duplicate_an.js
@@ -0,0 +1,17 @@
+let a=[1,2,4,5,7,1,5];
+let b =[];
+
+var dup = function ()
+{
+	let len = a.length;
+	 for (var i =0;i<len;i++)
+	 {
+	 	if(b.indexOf(a[i]) === -1)
+	 	{
+	 		b.push(a[i]);
+	 	}
+	 }
+console.log(b);
+}
+
+dup();
